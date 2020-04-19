d4342afae47b036b200fc5a99ca015e01976ecc5
AlexanderWinkelmeier <41574342+AlexanderWinkelmeier@users.noreply.github.com>
2020-04-19 13:24:45 +0200

    Update books.py

--- books_crawler/spiders/books.py
+++ books_crawler/spiders/books.py
@@ -8,11 +8,7 @@ class BooksSpider(CrawlSpider):
     start_urls = ['http://books.toscrape.com']
 
     rules = (Rule(LinkExtractor(), callback='parse_page', follow=False),)
-            # LinkExtractor extrahiert alle Links aus einer Webpage
-            # erlaubt hierbei nur links mit 'music' in der Url duch "allow='music" als Argument des LinkExtractors
-            # dann wird darüber mit parse_page geparsed
-            # dann wird auf die nächste Seite gegangen (mit follow = True)
-            # usw.
+           
     def parse_page(self, response):
         yield {'URL': response.url}
         
